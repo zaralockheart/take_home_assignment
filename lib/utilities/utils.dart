162e57d17a448f48b4ae133644714f9af9268ee1
yuzuriha <zaralockheart@gmail.com>
2020-05-18 10:14:41 +0800

    remove unused function

--- lib/utilities/utils.dart
+++ lib/utilities/utils.dart
@@ -1,13 +1,3 @@
 import 'package:flutter/material.dart';
 
-extension Context on BuildContext {
-  num getItemHeight(Orientation orientation) {
-    if (orientation == Orientation.portrait) {
-      return MediaQuery.of(this).size.width / 2 - 40;
-    }
-
-    return MediaQuery.of(this).size.width / 5 - 40;
-  }
-}
-
 bool isPortrait(Orientation orientation) => orientation == Orientation.portrait;
